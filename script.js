826995307cadbe2f2444a2e3cc9f64e7a271bdc2
Chris Kim <148381481+chriskgbc@users.noreply.github.com>
2026-01-28 19:16:28 +0000

    Exercise 2 complete

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function preload(){
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 650);
 }
 
 function draw() {
@@ -19,11 +19,25 @@ function draw() {
   text("Canada's Exports", 200, 40);
 
   // Iterate through the list and place elements
+
+
+
   Object.values(list).map((item, index) => {
-    rect(50, index * 30 + 70, item.amount, 10);
+    fill(item.color);
+    rect(50, index * 30 + 70, item.amount / 180 * 300, 10);
+
+    // 180 / 180 = 100%
+    // 37 / 180 = 21%
+
     textSize(10);
     textAlign("left");
+    fill("black");
     text(item.name, 50, index * 30 + 65);
+
+    textSize(20);
+    textAlign("right");
+    text(item.icon, 45, index * 30 + 75);
+
   });
 
 }
